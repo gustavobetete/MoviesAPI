d32a10f09f164cf9dd4775557665caf79664a948
Gustavo Betete <gustavobetete@hotmail.com>
2022-05-12 15:17:43 -0300

    Adicionando classes de exceptions

---
+++ ProjetoLocadora/src/main/java/com/projetolocadora/exceptions/objectNotFound/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.projetolocadora.exceptions.objectNotFound;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
